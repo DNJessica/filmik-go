4cc8a789c3cc831b2cd485f3fcfe20c429021ab2
Dzhesika Nimets <dzhesika.nimets@student.uj.edu.pl>
2021-05-01 20:32:54 +0300

    changed structure of the project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 ﻿import React from 'react';
-import logo from './logo.png';
-import WH from './WH.png';
-import RH from './RH.png';
+import logo from './components/images/logo.png';
+import whiteHeart from './components/images/whiteHeart.png';
+import redHeart from './components/images/redHeart.png';
 import './App.css';
 import * as axios from "axios";
 import FlatList from 'flatlist-react';
-import { icon } from '@fortawesome/fontawesome-svg-core';
-import Star from './Star.png'
-import finder from './Finder.png'
+import star from './components/images/star.png'
 import Autocomplete from 'react-autocomplete';
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
+        this.myRef = React.createRef() 
         this.state = {
             film: null,
             cdm: true,
@@ -23,7 +22,6 @@ class App extends React.Component {
             popular: true,
             pop: true,
             fav: false,
-            icon: WH,
             search: "",
             detail: false,
             over: 1,
@@ -49,9 +47,8 @@ class App extends React.Component {
                 favorites: []
             })
         }
-        console.log("ok")
-        let ep ='https://api.themoviedb.org/3/movie/popular?api_key=48c5285ea2a448984c23e818f1beece2&language=uk&page='+this.value
-        axios.get(ep)
+        let endpoint ='https://api.themoviedb.org/3/movie/popular?api_key=48c5285ea2a448984c23e818f1beece2&language=uk&page='+this.value
+        axios.get(endpoint)
             .then((response) => {
                 this.max_val=response.data.total_pages
                 this.setState({
@@ -70,7 +67,6 @@ class App extends React.Component {
     pop_value;
 
     getDetails(text) {
-
         this.setState({
             detail: true,
             film: text,
@@ -78,9 +74,8 @@ class App extends React.Component {
             fav: false
         })
 
-        let ep = 'https://api.themoviedb.org/3/movie/' + text.id + '/recommendations?api_key=48c5285ea2a448984c23e818f1beece2&language=uk'
-        axios.get(ep).then((response) => {
-            console.log(response.data.results)
+        let endpoint = 'https://api.themoviedb.org/3/movie/' + text.id + '/recommendations?api_key=48c5285ea2a448984c23e818f1beece2&language=uk'
+        axios.get(endpoint).then((response) => {
             this.setState({
                 similar: response.data.results
             })
@@ -90,56 +85,48 @@ class App extends React.Component {
 
 
     addToFavorites = async (film) => {
-        let t = JSON.stringify(film)
-        let r = JSON.stringify(this.state.favorites)
-        if (r.indexOf(t) === -1) {
-            console.log(film.title)
+        let filmJSON = JSON.stringify(film)
+        let favoritesJSON = JSON.stringify(this.state.favorites)
+        if (favoritesJSON.indexOf(filmJSON) === -1) {
            await this.setState({
                 favorites: this.state.favorites.concat(film)
             })
             localStorage.setItem('Films', JSON.stringify(this.state.favorites))
         }
         else {
-            console.log("delete"+film.title)
 
             await this.setState({
                 favorites: this.state.favorites.filter(item => JSON.stringify(item) !== JSON.stringify(film))
             })
-           await console.log(this.state.favorites)
             localStorage.setItem('Films', JSON.stringify(this.state.favorites))
         }
     }
 
     heartIcon(film) {
-        let t = JSON.stringify(film)
-        let r = JSON.stringify(this.state.favorites)
-        if (r.indexOf(t) === -1) {
-          return  <img src={WH} className="WHeart" onClick={() => this.addToFavorites(film)} />
+        let filmJSON = JSON.stringify(film)
+        let favoritesJSON = JSON.stringify(this.state.favorites)
+        if (favoritesJSON.indexOf(filmJSON)===-1) {
+          return  <img src={whiteHeart} className="WHeart" onClick={() => this.addToFavorites(film)} />
         }
         else {
-            console.log("R"+film.title)
-            return <img src={RH} className="RHeart" onClick={() => this.addToFavorites(film)} />
+            return <img src={redHeart} className="RHeart" onClick={() => this.addToFavorites(film)} />
         }
     }
 
     heartIcon2(film) {
-        let t = JSON.stringify(film)
-        let r = JSON.stringify(this.state.favorites)
-        if (r.indexOf(t) === -1) {
-            return <img src={WH} className="WHeart2" onClick={() => this.addToFavorites(film)} />
+        let filmJSON = JSON.stringify(film)
+        let favoritesJSON = JSON.stringify(this.state.favorites)
+        if (favoritesJSON.indexOf(filmJSON)===-1) {
+            return <img src={whiteHeart} className="WHeart2" onClick={() => this.addToFavorites(film)} />
         }
         else {
-            console.log("R" + film.title)
-            return <img src={RH} className="RHeart2" onClick={() => this.addToFavorites(film)} />
+            return <img src={redHeart} className="RHeart2" onClick={() => this.addToFavorites(film)} />
         }
     }
 
-    ic() {
-        return RH
-    }
+    
    
 
-    count = 0;
 
     renderFilms = (film) => {
         this.count = this.count + 1;
@@ -147,11 +134,11 @@ class App extends React.Component {
         let path = 'http://image.tmdb.org/t/p/w500' + film.poster_path
         return <div className="FilmFrame"
             ><div>
-                <img className={this.state.over != film.id ? "Poster" : "oPoster"} src={path} onClick={() => this.getDetails(film)}
+                <img className={this.state.over !== film.id ? "Poster" : "oPoster"} src={path} onClick={() => this.getDetails(film)}
                     onMouseEnter={() => this.setState({ over: film.id })}
                     onMouseLeave={() => this.setState({ over: 1 })} /></div>
-            <div className="VoteStar"><img src={Star} className="Star" />
-                <div className="Average"><span >{film.vote_average}</span></div>
+            <div className="VoteStar"><img src={star} className="Star" />
+                <div className="Average"><span >{film.vote_average.toFixed(1)}</span></div>
             </div>
             <div className="Title"><span>{film.title}</span><div className="Like">{this.heartIcon(film)}</div></div>
            </div>
@@ -165,28 +152,25 @@ class App extends React.Component {
     }
 
     genresList(genId) {
-        let s=''
+        let genString=''
         for (let id in genId) {
             const item = genId[id];
             for (let idx in this.state.genres.data.genres) {
                 const it = this.state.genres.data.genres[idx]
                 if(it.id===item)
-                s = s + it.name +", "
+                genString = genString + it.name +", "
             }
             
         }
-        let l = s.length;
-        let t = s.substring(0, l-2)
-        return t
+        let genSubstring = genString.substring(0, genString.length - 2)
+        return genSubstring
     }
 
     change(text) {
         this.setState({ val: text })
-        console.log(text)
-        let s = 'https://api.themoviedb.org/3/search/movie?api_key=48c5285ea2a448984c23e818f1beece2&language=uk&query=' + text
-        axios.get(s)
+        let endpoint = 'https://api.themoviedb.org/3/search/movie?api_key=48c5285ea2a448984c23e818f1beece2&language=uk&query=' + text
+        axios.get(endpoint)
             .then((response) => {
-                console.log(response.data.result)
                 this.setState({
                     founds: response.data.results,
                     isFetching: true
@@ -195,16 +179,13 @@ class App extends React.Component {
     }
  
 
-    Search(e) {
-        this.setState({ val: e })
-        console.log()
+    Search(filmName) {
+        this.setState({ val: filmName })
         this.setState({ popular: true, pop: false, fav: false })
-        console.log(e);
-        if (e!== "") {
-            let s = 'https://api.themoviedb.org/3/search/movie?api_key=48c5285ea2a448984c23e818f1beece2&language=uk&query=' + e
-            axios.get(s)
+        if (filmName!== "") {
+            let endpoint = 'https://api.themoviedb.org/3/search/movie?api_key=48c5285ea2a448984c23e818f1beece2&language=uk&query=' + filmName
+            axios.get(endpoint)
                 .then((response) => {
-                    console.log(response.data)
                     this.setState({
                         data: response.data.results,
                         isFetching: true
@@ -216,7 +197,6 @@ class App extends React.Component {
     mainView() {
         if (this.state.detail) {
             let path = 'http://image.tmdb.org/t/p/w500' + this.state.film.poster_path
-            console.log("1")
             return <div><div><div className="TitleText"><span>{this.state.film.title}</span></div>
                 <div> <div className="Picture"> <img className="PictureImg" src={path} /><div className="Like2">{this.heartIcon2(this.state.film)}</div> </div><div className="Descr">
                     <div className="TextInForm1"><span className="description">Дата виходу:</span></div>
@@ -240,7 +220,6 @@ class App extends React.Component {
             
         }
         else {
-            console.log("2")
             return <div><FlatList
                 list={this.state.popular ? this.state.data : this.state.favorites}
                 refreshing={this.state.isFetching}
